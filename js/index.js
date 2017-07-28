9c1262ed5c9fdca69844d4de1a0ceccb69f19e1c
Nazar Kyrychenko <kyrychenko.nazar@gmail.com>
2017-07-28 14:17:06 +0000

    fff

--- js/index.js
+++ js/index.js
@@ -1,146 +1,53 @@
-function print(text) {
-    document.write(text + '<br>');
-}
+// Simple
+function fibonacci(number) {
 
-function printArr(arr) {
-    print('Array size is: ' + arr.length);
-    for (var i = 0; i < arr.length; i++) {
-       print(i + 1 + ' ' + arr[i]); 
-    }    
-}
+    var past_first = 0, past_second = 1, next = 1;
 
-function printArrV2(arr) {
-    print('Array size is: ' + arr.length);
-    arr.forEach(function(el, i) {
-        print(i + 1 + ' ' + el);
-    });    
-}
-
-function printDict(dict) {
-    for (var key in dict) {
-        print(key + ': ' + dict[key]);
+    for(var i = 2; i <= number; i++) {
+        next = past_first + past_second;
+        past_first = past_second;
+        past_second = next;
     }
+    return next;
+};
+
+document.write('Result Simple  = ');
+document.write(fibonacci(prompt('Enter number')));
+
+
+
+
+
+
+
+
+// LoopAndArray
+function fibonacci(number) {
+
+	var sequence = [1, 1];
+
+	for (var i = 2; i < number; i++) {
+		sequence[i] = sequence[i-1]+ sequence[i-2];
+	}
+
+	return sequence[number-1];
+}
+document.write('Result LoopAndArray = ');
+document.write(fibonacci(prompt('Enter number')));
+
+
+
+//recurtion
+function fibonacci(number) {
+
+	if (number < 1)
+		return 0;
+
+	if (number <= 2)
+		return 1;
+
+   return fibonacci(number - 1) + fibonacci(number - 2);
 }
 
-
-let title,
-    price,
-    description,
-    image;
-    
-    
-    
-    
-let goodTitles = [];
-
-goodTitles.push('Mercedes GL500');
-goodTitles.push('BMW X6');
-goodTitles.push('Tesla Model X');
-
-goodTitles[3] = 'ZAZ Lanos';
-goodTitles.push('Audi TT');
-
-printArrV2(goodTitles);
-
-
-let prices = [60000, 90000, 150000, 10000, 50000];
-
-printArrV2(prices);
-
-printCarsAndPrices(goodTitles, prices);
-
-function printCarsAndPrices(cars, prices) {
-    for (var i = 0; i < cars.length; i++ ) {
-        print(cars[i] + ': ' + prices[i]);
-    }
-}
-
-let cars = {};
-
-cars['fastest'] = 'Tesla Model X';
-cars['cheapest'] = 'ZAZ Lanos';
-cars['largest'] = 'Mercedes GL500';
-
-print('Fastest car is: ' + cars['fastest']);
-print(cars.length);
-printDict(prices);
-
-let mb = {};
-
-mb.title = 'Mercedes GL500';
-mb.price = 90000;
-mb.description = 'Cool car';
-mb.color = 'Black';
-
-function carRun() {
-    print(this.title + ' is running!!!!');
-}
-
-mb.run = carRun;
-
-print(mb.description);
-
-mb.run();
-
-let bmw = {};
-
-bmw.title = 'BMW X6';
-bmw.price = 100000;
-bmw.description = 'Very Cool car';
-bmw.color = 'Red';
-
-bmw.run = carRun;
-
-bmw.run();
-
-let audi = {
-    title: 'Audi TT',
-    price: 50000,
-    description: 'Cabrio',
-    color: 'Red',
-    run: carRun
-}
-
-audi.run();
-
-
-function Car(title, price, description, color) {
-    this.title = title;
-    this.price = price;
-    this.description = description;
-    this.color = color;
-    this.run = function () {
-        print(this.title + ' is running');
-    };
-    return this;
-}
-
-let zaz = new Car('ZAZ Lanos', 10000, 'Everyday car', 'Gray');
-
-zaz.run();
-
-class NewCar {
-    constructor(title, price, description, color) {
-        this.title = title;
-        this.price = price;
-        this.description = description;
-        this.color = color;
-    }
-    run() {
-        print(this.title + ' is running')
-        
-    };
-    
-}
-
-let lr = new NewCar('LR Defender', 50000, 'Off-road', 'Silver');
-lr.run();
-
-
-
-let lrRed = Object.create(lr);
-lrRed.color = 'Red'
-lrRed.title = 'LR Def RED'
-
-lrRed();
-
+document.write('Result recurtion = ');
+document.write(fibonacci(prompt('Enter number')));
